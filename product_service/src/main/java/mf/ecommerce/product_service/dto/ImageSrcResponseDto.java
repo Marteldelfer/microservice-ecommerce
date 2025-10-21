99f99291768149467e1ce937e70396c42d08f38a
Marteldelfer <marteldelfer@gmail.com>
2025-10-21 20:41:28 -0300

    add getter to responseDtos

--- product_service/src/main/java/mf/ecommerce/product_service/dto/ImageSrcResponseDto.java
+++ product_service/src/main/java/mf/ecommerce/product_service/dto/ImageSrcResponseDto.java
@@ -1,10 +1,12 @@
 package mf.ecommerce.product_service.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.UUID;
 
 @Builder
+@Getter
 public class ImageSrcResponseDto {
 
     private UUID id;
--- product_service/src/main/java/mf/ecommerce/product_service/dto/TagResponseDto.java
+++ product_service/src/main/java/mf/ecommerce/product_service/dto/TagResponseDto.java
@@ -1,10 +1,12 @@
 package mf.ecommerce.product_service.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.UUID;
 
 @Builder
+@Getter
 public class TagResponseDto {
 
     private UUID id;
